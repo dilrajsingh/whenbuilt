9b9f0fdb6ff2cddf8a0433d780311a74af5ef05c
Dilraj Singh <dilraj1.singh@gmail.com>
2022-05-09 12:29:53 -0400

    First commit
    added vscode path to .gitignore.
    
    added .nvmrc file

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { APP_BOOTSTRAP_LISTENER, Component, OnInit } from '@angular/core';
+import * as atlas from 'azure-maps-control';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'whenbuilt';
+
+  constructor() {
+
+  }
+  ngOnInit(): void {
+
+  }
+
+  getPosition(): Promise<any>{
+    return new Promise((resolve, reject) => {
+
+      navigator.geolocation.getCurrentPosition(resp => {
+
+          resolve({lng: resp.coords.longitude, lat: resp.coords.latitude});
+        },
+        err => {
+          reject(err);
+        });
+    });
+
+  }
+}
